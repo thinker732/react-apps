784463bc3bdbcedfc2f3bfbdeb9a837a09df6b89
Savitar <baedsavitar@gmail.com>
2021-08-18 08:54:32 +0200

    add hooks

--- robots/src/containers/app/App(class).js
+++ robots/src/containers/app/App(class).js
@@ -1,45 +1,59 @@
-import React, {useState,useEffect} from 'react'
+import React, {Component} from 'react'
 import CardList from '../../components/cardlist/CardList'
 import SearchBox from '../../components/searchbox/SearchBox'
 import Scroll from '../../components/scroll/scroll'
 import ErrorBoundry from '../../components/error/ErrorBoundry.js'
 import './App.css'
 
-function App(){
+class App extends Component{
 
-    const [robots,setRobots]=useState([])
-    const [searchfield,setSearchfield]=useState('')
+    constructor(){
+        super()
+        this.state={
+            robots:[],
+            searchfield: ''
+        }
+       
+    }
 
-    useEffect(()=>{
+    componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then((response)=> response.json())
         .then((users)=>{
-              setRobots(users)
-         
-        },[])
-    },[])
+            this.setState({
+                  robots:users
+             })
+        })
 
-   
-    const onSearchChange=(event)=>{
-        setSearchfield(event.target.value)
     }
 
-     const filterRobots=robots.filter(robot =>{
-        return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
-    })
+    onSearchChange=(event)=>{
+        this.setState({
+            searchfield:event.target.value
+        })
+        
+    }
 
+    render(){
+        const {robots,searchfield}=this.state
+        const filterRobots=robots.filter(robot =>{
+            return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+        })
 
-        return (!robots.length)?(
+        if(!robots.length){
+            return(
                 <div className="tc">
                     <h1 className="f2">Robot React-App</h1>
                     <p className="f2"> Loading...</p>
             </div>
-            ):
-            (
+            )
+        }
+
+        return(
             <div className="tc">
                 <h1 className="f2">Robot React-App</h1>
 
-                    <SearchBox searchChange={onSearchChange}/>
+                    <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
                         <ErrorBoundry>
                         <CardList robots={filterRobots}/>
@@ -48,8 +62,8 @@ function App(){
             </div>
         
             )
- }
+    }
    
-
+}
 
 export default App;
